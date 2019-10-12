21d48a33db3f28d1242b5a9ce5265cae4d4a18f1
kirigayakazuto42@gmail.com <kirigayakazuto42@gmail.com>
2019-10-12 22:06:51 +0700

    fix bug noti

--- server/noti.js
+++ server/noti.js
@@ -10,23 +10,25 @@ fs.watchFile(filePath, async () => {
         var newEp = newFile[0]
         var parent_id = newEp.anime_id
         var meta_key = "follow"
-        var usermeta = await UserMeta
-            .findOne({ parent_id, meta_key, $or: [{ meta_value: "watching" }, { meta_value: "considering" }] })
-        if (usermeta) {
-            var user_id = usermeta.user_id
+        var usermetas = await UserMeta
+            .find({ parent_id, meta_key, $or: [{ meta_value: "watching" }, { meta_value: "considering" }] })
+        if (usermetas.length > 0) {
             var anime_id = parent_id
-            var episode_id = newEp.episode_id
             var anime = await Anime.findOne({ anime_id }, { _id: 0 }).select('title thumbnail')
             var cover = anime.thumbnail
+            var episode_id = newEp.episode_id
             var message = `Anime: <strong>${anime.title}</strong> have a new episode uploaded.`
             var thumbnail = newEp.thumbnail
-            var isHas = await Noti.findOne({ user_id, anime_id, episode_id })
-            if (!isHas) {
-                await Noti.create({ user_id, anime_id, episode_id, message, thumbnail, cover })
-            }
-            var total = await Noti.countDocuments({ user_id })
-            if (total > 10) {
-                await Noti.deleteOne({ user_id })
+            for (var usermeta of usermetas) {
+                var user_id = usermeta.user_id
+                var isHas = await Noti.findOne({ user_id, anime_id, episode_id })
+                if (!isHas) {
+                    await Noti.create({ user_id, anime_id, episode_id, message, thumbnail, cover })
+                }
+                var total = await Noti.countDocuments({ user_id })
+                if (total > 10) {
+                    await Noti.deleteOne({ user_id })
+                }
             }
         }
     } catch (err) {
