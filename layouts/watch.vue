f5360066b755259058ea7afac2f8d0e87c0aa73a
kirigayakazuto42@gmail.com <kirigayakazuto42@gmail.com>
2020-02-21 22:47:44 +0700

    update alert

--- layouts/watch.vue
+++ layouts/watch.vue
@@ -5,6 +5,7 @@
     </client-only>
     <v-content>
       <v-container fuild grid-list-md>
+        <v-alert type="info" elevation="2" v-if="settings.noti">{{settings.noti}}</v-alert>
         <DialogSignIn />
         <SnackBar />
         <nuxt />
@@ -17,11 +18,15 @@
 import DialogSignIn from "@/components/template/dialog/requireSignIn";
 import SnackBar from "@/components/template/SnackBar";
 import AppBar from "@/components/main/AppBarWatch";
+import { mapState } from "vuex";
 export default {
   components: {
     AppBar,
     DialogSignIn,
     SnackBar
+  },
+  computed: {
+    ...mapState(["settings"])
   },
   watch: {
     "$store.state.auth.isLogin"(val) {
